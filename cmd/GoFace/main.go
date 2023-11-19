6202792f5997a56988e57f5eb6bdda0e67c72bce
D-Mielewczyk <MDDawid0323@gmail.com>
2023-11-19 17:32:10 +0100

    add the ability to process all photos

--- cmd/GoFace/main.go
+++ cmd/GoFace/main.go
@@ -4,15 +4,32 @@ import (
     "github.com/D-Mielewczyk/GoFace/internal/detection"
     "log"
     "os"
+    "path/filepath"
 )
 
 func main() {
     log.Println("main")
 
     if len(os.Args) < 2 {
-        log.Fatalf("Usage: %s <image path>", os.Args[0])
+        log.Fatalf("Usage: %s <image path> or %s all", os.Args[0], os.Args[0])
     }
-    imagePath := "images/" +os.Args[1]
+    arg := os.Args[1]
 
-    detection.DetectFace(imagePath, "")
+    if arg == "all" {
+        files, err := os.ReadDir("images")
+        if err != nil {
+            log.Fatalf("Cannot read images directory: %v", err)
+        }
+
+        for _, file := range files {
+            if !file.IsDir() {
+                imagePath := filepath.Join("images", file.Name())
+                log.Printf("Processing file: %s", imagePath)
+                detection.DetectFace(imagePath, "")
+            }
+        }
+    } else {
+        imagePath := filepath.Join("images", arg)
+        detection.DetectFace(imagePath, "")
+    }
 }
